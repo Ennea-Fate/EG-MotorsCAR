a406d2c56882e5bf4fc81a2457af0ab530ac1c78
Ennea-Fate <rushero@live.ru>
2019-12-29 13:41:22 +0300

    Added price page and 2 minor text mistakes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'job_opening/vacancy'
+  get 'price', to: 'main_page#price'
+  get 'vacancy', to: 'job_opening#vacancy'
   devise_for :users
   get 'main_page/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
